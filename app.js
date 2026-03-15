8c920e73933990383fa64a7e3f375e611bcdb5b5
vikas-cse123 <sagarraothakur2@gmail.com>
2026-03-15 12:06:33 +0000

    Use dotenv package

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -51,7 +52,7 @@ app.use("/auth", authRoutes);
 
 
 app.get("/",(req,res) => {
-  res.end("abcdefghijklmnopqrstuvwxyz1234567890.")
+  res.end("io_io_io_io_io_io_io_io")
 
 })
 
